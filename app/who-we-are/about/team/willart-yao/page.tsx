27afea92b351b36fc885f1cbc78c634528d1dd5a
yaowillart-design <yao.willart@gmail.com>
2025-10-21 19:47:15 +0800

    Revert "Roger (#10)"
    This reverts commit 3d17afe4b9a1972b49f232777267a9ed2b5a6972.

---
+++ app/who-we-are/about/team/willart-yao/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Willart Yao</h1>
+      <p>
+        Please add the profile content for Willart Yao here.
+      </p>
+    </div>
+  );
+}
