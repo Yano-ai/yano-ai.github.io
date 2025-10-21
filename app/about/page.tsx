b3eeb837ad57c19d1f0aacc6d9fe2f87c7ac62b7
Willart Yao <yao.willart@gmail.com>
2025-10-21 21:04:47 +0800

    change profile pic

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -139,7 +139,7 @@ const WhoWeArePage = () => {
       name: 'Willart Yao',
       title: 'AI-Powered Web Developer',
       bio: `An emerging web developer, Willart Yao brings almost a year of expertise in developing dynamic and scalable web applications, with a specialisation in advanced AI integration.`,
-      image: 'https://placeholder.pics/svg/300x300/DEDEDE/555555/Willart%20yao',     social: { linkedin: '#', github: 'https://github.com/willartyao-design' }
+      image: 'out/image-aiko/willart yao.png',     social: { linkedin: '#', github: 'https://github.com/willartyao-design' }
     }
   ];
 
